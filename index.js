15a4ade591b9d084233735314effc241eac9e161
Wyatt Ford <wyattford04@gmail.com>
2025-06-03 10:55:18 -0700

    refactor: clean up imports and add comments for clarity of features

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+// General imports
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const app = express();
 
+// Load exported routes with Express router
 const { router: accountRouter } = require('./account');
 const { router: reportsRouter } = require('./reports');
 const { router: discussionRouter } = require('./discussion');
-const path = require('path');
-const fs = require('fs');
-const marked = require('marked');
 
-const app = express();
-
+// Prepare the Express app
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
@@ -27,17 +26,20 @@ app.get('/', (req, res) => {
     res.send('This is the backend service for UW Wildlife. Visit the documentation at <a href="/docs">/docs</a> for more information.');
 });
 
+// Health check endpoint
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'OK' });
 });
 
+// API documentation endpoint
 app.get('/docs', (req, res) => {
     res.sendFile('Documentation.html', {root: __dirname })
 });
 
-
+// Default port for server
 const PORT = 19005;
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`UW Wildlife backend running on port ${PORT}`);
 });
