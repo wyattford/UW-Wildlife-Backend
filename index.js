ea46ab4a0bfa11863205f581be3daa900e94ce3c
Wyatt Ford <wyattford04@gmail.com>
2025-06-02 23:49:43 -0700

    Add documentation endpoint to serve API documentation in HTML format

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 const { router: accountRouter } = require('./account');
 const { router: reportsRouter } = require('./reports');
 const { router: discussionRouter } = require('./discussion');
+const path = require('path');
+const fs = require('fs');
+const marked = require('marked');
 
 const app = express();
 
@@ -21,11 +24,22 @@ app.use('/discussion', discussionRouter);
 
 // Default endpoint
 app.get('/', (req, res) => {
-    res.send('This is the backend service for UW Wildlife.');
+    res.send('This is the backend service for UW Wildlife. Visit the documentation at <a href="/docs">/docs</a> for more information.');
 });
 
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'OK' });
+});
+
+app.get('/docs', (req, res) => {
+    const docPath = path.join(__dirname, 'Documentation.md');
+    fs.readFile(docPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(404).send('Documentation not found.');
+        }
+        const html = marked.parse(data);
+        res.type('text/html').send(html);
+    });
 });
 
 
